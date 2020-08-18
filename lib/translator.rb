45f0b8bc9fdbadefd82d5db2392ac9bd2c7852b2
megan <meggsnbacon777@gmail.com>
2020-08-18 06:36:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@
  require 'yaml'
 def load_library(file_path)
     newhash=.YAML file_load(file_path)
-    file_path each do [|key,value|]
+    file_path each do |key,value|
     newhash[key]={}
     newhash[key][:japanese]=value[0]
     newhash[key][:english]=value[1]
