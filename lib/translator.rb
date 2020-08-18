a022de8d5723eb2abc0696e46ff8b517eec71491
megan <meggsnbacon777@gmail.com>
2020-08-18 04:49:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ list2hash = YAML.load_file(file_path)
 results={"put_meaning"=>{},"out_emo"=>{}}
   list2hash.each do |key,value|
     results["put_meaning"][value[1]]=key
-    results["out_emo"][key[0]]=value# code goes here
+    results["out_emo"][key[0]]=value # code goes here
 end
 return results
 def get_japanese_emoticon
