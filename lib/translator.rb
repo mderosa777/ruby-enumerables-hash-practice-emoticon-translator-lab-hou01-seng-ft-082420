76654bdbbc15752d8f15958d70d8b463ead267f6
megan <meggsnbacon777@gmail.com>
2020-08-18 04:46:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 # require modules here
 
-def load_library
-  # code goes here
+ require 'yaml'
+def load_library(file_path)
+list2hash = YAML.load_file(file_path)
+
+results=>{"put_meaning"=>{},"out_emo"=>{}}
+  list2hash.each do |key,value|
+    results["put_meaning"]value[1]]=key
+    results["out_emo"][key[0]]=value# code goes here
 end
 
 def get_japanese_emoticon
