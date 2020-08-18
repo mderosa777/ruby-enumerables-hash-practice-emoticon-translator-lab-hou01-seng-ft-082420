d61174cfdc89acb88173ee92a6e9eff1959005ec
megan <meggsnbacon777@gmail.com>
2020-08-18 09:07:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,7 @@ def get_english_meaning(file_path,japanese_emoticon)
   library=load_library(file_path)
   library.each do |key,value|
     if library[key][:japanese]==japanese_emoticon
-      return value
+      return key
     end 
   end  
   "Sorry, that emoticon was not found" # code goes here
